3dd148899b54673cc68fd2698cf79e5c89cd57e1
Nojze <niklasryd01@gmail.com>
2020-06-28 19:47:25 +0200

    small typo update

--- niklas/src/error/Error.tsx
+++ niklas/src/error/Error.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components/macro";
 
 const Error = () => {
-  return <>Error 404</>;
+  return <>This page does not exist...</>;
 };
 
 export default Error;
