dcb410657ad07da292da0955b03c1e18fbeffa11
Nojze <niklasryd01@gmail.com>
2020-10-07 11:21:05 +0200

    Added better mobile nav

--- src/components/NavigationBar/MobileNavigationBar.tsx
+++ src/components/NavigationBar/MobileNavigationBar.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 import { homeURL, contactURL } from "../../config";
 
 const activeStyle = {
-  fontWeight: 500,
+  fontWeight: 600,
 };
 
 const MobileNavigationBar = () => {
@@ -19,17 +19,16 @@ const MobileNavigationBar = () => {
   };
 
   const StyledNavLink = ({ href, title, iconName }: any) => (
-    <li className="inline-flex align-center justify-center m-0">
+    <li className="inline-flex align-center justify-center m-0 border-b hover:bg-gray-100">
       <NavLink
-        className="flex flex-row align-center justify-center text-2xl p-0 py-6 w-full text-white text-center hover:text-opacity-75"
-        style={{ fontFamily: "BlinkMacSystemFont" }}
+        className="flex flex-row align-center justify-center text-2xl p-0 py-6 w-full text-center "
         activeStyle={activeStyle}
         to={href}
         title={title}
         onClick={closeMenu}
       >
         <i className={`${iconName} mr-4 self-center`} />
-        {title}
+        <span className="hover:underline">{title}</span>
       </NavLink>
     </li>
   );
@@ -37,16 +36,38 @@ const MobileNavigationBar = () => {
   return (
     <>
       <header
-        className="sticky flex justify-around items-center py-4 px-2 z-10"
-        style={{ backgroundColor: theme.colors.customBlack }}
+        className="sticky flex justify-around items-center py-4 px-2 z-10 border-b"
+        style={{
+          backgroundColor: isOpen
+            ? theme.colors.white
+            : theme.colors.customBlack,
+          transition: "all 0.3s ease",
+        }}
       >
-        <NavLink className="inline-flex" to={homeURL} title="Home">
+        <NavLink
+          className="inline-flex"
+          to={homeURL}
+          onClick={closeMenu}
+          title="Home"
+        >
           <img
             className="w-12 h-12 border rounded-full mr-4"
+            style={{
+              borderColor: isOpen
+                ? theme.colors.customBlack
+                : theme.colors.white,
+              transition: "all 0.3s ease",
+            }}
             src="/favicon.ico"
             alt="Niklas Rydkvist"
           />
-          <span className="text-xl self-center font-medium text-white uppercase tracking-widest m-0">
+          <span
+            className="text-xl self-center font-medium uppercase tracking-widest m-0"
+            style={{
+              color: isOpen ? theme.colors.customBlack : theme.colors.white,
+              transition: "all 0.3s ease",
+            }}
+          >
             Niklas Rydkvist
           </span>
         </NavLink>
@@ -58,10 +79,14 @@ const MobileNavigationBar = () => {
           className={`hamburger hamburger--spring p-1 ${
             isOpen ? "is-active" : ""
           }`}
+          style={{
+            color: isOpen ? theme.colors.customBlack : theme.colors.white,
+            transition: "all 0.3s ease",
+          }}
           type="button"
         >
           <span className="hamburger-box">
-            <span className="hamburger-inner"></span>
+            <span className="hamburger-inner hamburger.is-active-dark hamburger-inner-dark"></span>
           </span>
         </button>
       </header>
@@ -69,11 +94,11 @@ const MobileNavigationBar = () => {
         role="navigation"
         className="justify-center z-50 absolute bg-white w-full h-full"
         style={{
-          backgroundColor: theme.colors.lightBlack,
           transition: "all 0.3s ease",
           visibility: isOpen ? "visible" : "hidden",
           height: isOpen ? "100%" : "0%",
           opacity: isOpen ? "1000%" : "0%",
+          color: isOpen ? theme.colors.customBlack : theme.colors.white,
         }}
       >
         <ul className="relative flex flex-col m-0 p-0 list-none w-full">
