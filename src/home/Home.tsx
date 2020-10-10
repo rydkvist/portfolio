75892ce5595b280de0d86c6bbe182d3ea05f6e36
Nojze <niklasryd01@gmail.com>
2020-10-10 19:39:17 +0200

    wip

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -31,107 +31,121 @@ const Home = () => {
   const isMobile = useMediaMax(768);
 
   return (
-    <Container>
-      <TextContent>
-        <p style={{ fontSize: "2rem", textAlign: "center", display: "block" }}>
-          Hey there! I'm Niklas.
-          <br />
-          <span style={{ fontSize: "1.5rem" }}>
-            A software developer from Sweden, Stockholm.
-          </span>
-        </p>
-      </TextContent>
-      <div style={{ width: "28rem" }}>
-        <img
-          width={isMobile ? "50%" : "100%"}
-          src="/favicon.ico"
-          alt="Niklas Rydkvist"
-        />
+    <>
+      <div className="container max-w-screen-lg mx-auto bg-red-300">
+        <div className="flex flex-col justify-center items-center">
+          <div>
+            <img className="w-32 h-32 rounded-full" src="/favicon.ico" />
+          </div>
+          <h1 className="text-3xl">I'm Niklas Rydkvist</h1>
+          <h4>A Software Developer</h4>
+        </div>
       </div>
-      <p
-        style={{
-          fontSize: "1.125rem",
-          textAlign: "center",
-        }}
-      >
-        Page is not finished yet. There is work in progress. <br />
-        <br /> The same thing counts for all the other pages! :)
-      </p>
-      <p
-        style={{
-          fontSize: "1.125rem",
-          textAlign: "center",
-        }}
-      >
-        Page is not finished yet. There is work in progress. <br />
-        <br /> The same thing counts for all the other pages! :)
-      </p>
-      <p
-        style={{
-          fontSize: "1.125rem",
-          textAlign: "center",
-        }}
-      >
-        Page is not finished yet. There is work in progress. <br />
-        <br /> The same thing counts for all the other pages! :)
-      </p>
-      <p
-        style={{
-          fontSize: "1.125rem",
-          textAlign: "center",
-        }}
-      >
-        Page is not finished yet. There is work in progress. <br />
-        <br /> The same thing counts for all the other pages! :)
-      </p>
-      <p
-        style={{
-          fontSize: "1.125rem",
-          textAlign: "center",
-        }}
-      >
-        Page is not finished yet. There is work in progress. <br />
-        <br /> The same thing counts for all the other pages! :)
-      </p>{" "}
-      <p
-        style={{
-          fontSize: "1.125rem",
-          textAlign: "center",
-        }}
-      >
-        Page is not finished yet. There is work in progress. <br />
-        <br /> The same thing counts for all the other pages! :)
-      </p>{" "}
-      <p
-        style={{
-          fontSize: "1.125rem",
-          textAlign: "center",
-        }}
-      >
-        Page is not finished yet. There is work in progress. <br />
-        <br /> The same thing counts for all the other pages! :)
-      </p>
-      <SocialMedia marginTop={10}>
-        <SocialMedia.Item
-          key="social-media-linkedin"
-          icon={<i className="fab fa-linkedin fa-lg"></i>}
-          link={linkedInURL}
-          altText="LinkedIn - Niklas Rydkvist"
-        />
-        <SocialMedia.Item
-          key="social-media-github"
-          icon={<i className="fab fa-github fa-lg"></i>}
-          link={gitHubURL}
-          altText="GitHub - Nojze"
-        />
-        <SocialMedia.Item
-          key="social-media-twitter"
-          icon={<i className="fab fa-twitter fa-lg"></i>}
-          link={twitterURL}
-          altText="Twitter"
-        />
-      </SocialMedia>
-    </Container>
+
+      <Container style={{ display: "none" }}>
+        <TextContent>
+          <p
+            style={{ fontSize: "2rem", textAlign: "center", display: "block" }}
+          >
+            Hey there! I'm Niklas.
+            <br />
+            <span style={{ fontSize: "1.5rem" }}>
+              A software developer from Sweden, Stockholm.
+            </span>
+          </p>
+        </TextContent>
+        <div style={{ width: "28rem" }}>
+          <img
+            width={isMobile ? "50%" : "100%"}
+            src="/favicon.ico"
+            alt="Niklas Rydkvist"
+          />
+        </div>
+        <p
+          style={{
+            fontSize: "1.125rem",
+            textAlign: "center",
+          }}
+        >
+          Page is not finished yet. There is work in progress. <br />
+          <br /> The same thing counts for all the other pages! :)
+        </p>
+        <p
+          style={{
+            fontSize: "1.125rem",
+            textAlign: "center",
+          }}
+        >
+          Page is not finished yet. There is work in progress. <br />
+          <br /> The same thing counts for all the other pages! :)
+        </p>
+        <p
+          style={{
+            fontSize: "1.125rem",
+            textAlign: "center",
+          }}
+        >
+          Page is not finished yet. There is work in progress. <br />
+          <br /> The same thing counts for all the other pages! :)
+        </p>
+        <p
+          style={{
+            fontSize: "1.125rem",
+            textAlign: "center",
+          }}
+        >
+          Page is not finished yet. There is work in progress. <br />
+          <br /> The same thing counts for all the other pages! :)
+        </p>
+        <p
+          style={{
+            fontSize: "1.125rem",
+            textAlign: "center",
+          }}
+        >
+          Page is not finished yet. There is work in progress. <br />
+          <br /> The same thing counts for all the other pages! :)
+        </p>{" "}
+        <p
+          style={{
+            fontSize: "1.125rem",
+            textAlign: "center",
+          }}
+        >
+          Page is not finished yet. There is work in progress. <br />
+          <br /> The same thing counts for all the other pages! :)
+        </p>{" "}
+        <p
+          style={{
+            fontSize: "1.125rem",
+            textAlign: "center",
+          }}
+        >
+          Page is not finished yet. There is work in progress. <br />
+          <br /> The same thing counts for all the other pages! :)
+        </p>
+        <SocialMedia marginTop={10}>
+          <SocialMedia.Item
+            key="social-media-linkedin"
+            icon={<i className="fab fa-linkedin fa-lg"></i>}
+            link={linkedInURL}
+            altText="LinkedIn - Niklas Rydkvist"
+          />
+          <SocialMedia.Item
+            key="social-media-github"
+            icon={<i className="fab fa-github fa-lg"></i>}
+            link={gitHubURL}
+            altText="GitHub - Nojze"
+          />
+          <SocialMedia.Item
+            key="social-media-twitter"
+            icon={<i className="fab fa-twitter fa-lg"></i>}
+            link={twitterURL}
+            altText="Twitter"
+          />
+        </SocialMedia>
+      </Container>
+    </>
   );
 };
 
