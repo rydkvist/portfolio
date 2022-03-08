a478b4e285bd78555a23f723bd654291a703bd1b
nojze <niklasryd01@gmail.com>
2022-03-08 22:05:14 +0100

    Setup a NextJS project with decent dependecies

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const APP_HOME_URL = '/';
+export const APP_CONTACT_URL = '/contact';
+export const APP_WEBSITE_URL = 'https://rydkvist.com';
+export const APP_TITLE = 'Niklas Rydkvist';
+export const APP_DESCRIPTION =
+  'Niklas Rydkvist is a software engineer based in Stockholm, Sweden. He is passionate about building software that improves the lives of people.';
+
+export const GITHUB_URL = 'https://github.com/Nojze';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/niklasrydkvist/';
+
+export const PERSONAL_EMAIL = 'niklas@rydkvist.com';
+export const PERSONAL_PHONE_NUMBER = '+46 70 377 11 04';
+export const PERSONAL_ADDRESS = 'Stockholm, Sweden';
