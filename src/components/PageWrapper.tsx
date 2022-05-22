d114fa7cb7a77f154966427f345dba5664ce0ba2
nojze <niklasryd01@gmail.com>
2022-05-23 00:08:51 +0200

    Add better documentation style to landing page

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -7,7 +7,9 @@ type PageWrapperProps = {
 export const PageWrapper = ({ children }: PageWrapperProps) => {
   return (
     <div className="container mx-8 md:mx-16 lg:mx-32 xl:mx-64">
-      <div className="flex flex-col min-h-screen py-8 items-center text-black dark:text-white">{children}</div>
+      <div className="flex flex-col min-h-screen py-8 items-center text-1xl leading-7 text-black dark:text-white">
+        {children}
+      </div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,36 +32,38 @@ const Landing = () => {
           />
         </div>
 
-        <p className="text-1xl leading-7 mt-6">
+        <h2 className="text-2xl w-full mt-8 mb-1">Who am I?</h2>
+        <p>
           I&apos;m a passionate and driven <span className="font-semibold">Software Engineer</span> from Stockholm,
           Sweden specializing in both <span className="font-semibold">web and app development</span>. I create
           enterprise end-to-end applications by building scalable architectures with{' '}
           <span className="font-semibold">great code quality</span> and{' '}
           <span className="font-semibold">excellent user experience (UX/UI)</span>.
           <br />
-          <br />
           I&apos;m <span className="font-semibold">very product-oriented</span> and like to discuss different business
           perspectives with the team in order to move the product forward in the right direction. Other than
           experimenting with new technologies I also invest some of my time into the stock market, working out,
           travelling and meeting new exciting people!
           <br />
-          <p className="mt-4 mb-2">Technologies I use day-to-day:</p>
-          {/* TODO: Maybe show the technologies with a scrolling carousel? Like when companies show sponsors etc */}
-          {/* TODO: Add correct programming languages */}
-          <ul className="flex flex-row justify-center flex-wrap text-center">
-            <TechnologyItem value="TypeScript" />
-            <TechnologyItem value="React" color="bg-cyan-400" />
-            <TechnologyItem value="React Native" color="bg-cyan-400" />
-            <TechnologyItem value="Swift" color="bg-orange-400" />
-            <TechnologyItem value="SwiftUI" color="bg-red-400" />
-          </ul>
-          <p className="mt-4 mb-2">Other technologies / frameworks / concepts I&apos;m experienced with:</p>
-          <p className="italic mt-1">
-            JavaScript, HTML5, CSS3, NextJS, TailwindCSS, Cypress, Jest, End-to-end testing, Unit testing, Storybook,
-            Git, CI/CD, TDD, APIs, Redux, Figma, User Accessibility, QA (Quality Assurance), SEO, Copywriting,
-            multi-threading programming, SocketIO, WebRTC, Google Cloud, Docker, Test Flight, Azure DevOps, AWS and
-            Netlify.
-          </p>
+        </p>
+
+        <h2 className="text-1xl text-2xl w-full mt-6 mb-2">My tech stack</h2>
+
+        {/* TODO: Maybe show the technologies with a scrolling carousel? Like when companies show sponsors etc */}
+        {/* TODO: Add correct programming languages */}
+        <ul className="flex flex-row justify-center flex-wrap text-center">
+          <TechnologyItem value="TypeScript" />
+          <TechnologyItem value="React" color="bg-cyan-400" />
+          <TechnologyItem value="React Native" color="bg-cyan-400" />
+          <TechnologyItem value="Swift" color="bg-orange-400" />
+          <TechnologyItem value="SwiftUI" color="bg-red-400" />
+        </ul>
+
+        <h2 className="text-2xl w-full mt-6 mb-2">More tech I&apos;m experienced with</h2>
+        <p className="italic">
+          JavaScript, HTML5, CSS3, NextJS, TailwindCSS, Cypress, Jest, End-to-end testing, Unit testing, Storybook, Git,
+          CI/CD, TDD, APIs, Redux, Figma, User Accessibility, QA (Quality Assurance), SEO, Copywriting, multi-threading
+          programming, SocketIO, WebRTC, Google Cloud, Docker, Test Flight, Azure DevOps, AWS and Netlify.
         </p>
       </>
     </PageWrapper>
