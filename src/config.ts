9b53387c75f7466f57148dc174702efff0a11cfc
nojze <niklasryd01@gmail.com>
2022-05-23 00:47:48 +0200

    Disable flags

--- src/config.ts
+++ src/config.ts
@@ -18,5 +18,5 @@ export const PERSONAL_EMAIL = 'niklas@rydkvist.com';
 export const PERSONAL_PHONE_NUMBER = '+46 70 377 11 04';
 export const PERSONAL_ADDRESS = 'Stockholm, Sweden';
 
-export const SHOW_INVESTMENTS_SUMMARY = true;
-export const SHOW_MUSIC_SUMMARY = true;
+export const SHOW_INVESTMENTS_SUMMARY = false;
+export const SHOW_MUSIC_SUMMARY = false;
