b538f444831b9cfb3068f278996d34e3b09377af
Nojze <niklasryd01@gmail.com>
2023-02-07 10:33:55 +0100

    Update profile picture

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,13 +23,7 @@ const Landing = () => {
 const ProfilePicture = () => {
   return (
     <div className="w-40 md:w-48 h-40 md:h-48 overflow-hidden rounded-full relative">
-      <Image
-        src="/images/me/NiklasMalmo.png"
-        alt="Picture of Niklas Rydkvist"
-        layout="fill"
-        objectFit="cover"
-        priority
-      />
+      <Image src="/images/me/Niklas.png" alt="Picture of Niklas Rydkvist" layout="fill" objectFit="cover" priority />
     </div>
   );
 };
