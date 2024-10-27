2c7f29b621fad66013d591e06b99a4210b342c75
Nojze <niklas@rydkvist.com>
2024-10-27 04:33:50 +0100

    fix: add icons

--- app-next/portfolio/app/_domains/sitewide/TabNavigationItem.tsx
+++ app-next/portfolio/app/_domains/sitewide/TabNavigationItem.tsx
@@ -1,16 +1,11 @@
 'use client';
 
+import { Icon, IconName } from '@/_components/Icon';
 import { cva } from 'class-variance-authority';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-type TabNavigationItemProps = {
-  label: string;
-  href: string;
-  icon: string;
-};
-
-export const TabNavigationItem = ({ href, icon, label }: TabNavigationItemProps) => {
+export const TabNavigationItem = ({ href, icon, label }: { label: string; href: string; icon: IconName }) => {
   const isExternal = href.startsWith('http');
   const pathname = usePathname();
   const isActive = pathname === href;
@@ -24,7 +19,9 @@ export const TabNavigationItem = ({ href, icon, label }: TabNavigationItemProps)
         rel="noreferrer"
         href={href}
       >
-        <span className="w-5 h-5">{icon}</span>
+        <span className="w-5 h-5">
+          <Icon name={icon} height={20} width={20} />
+        </span>
       </Link>
     </li>
   );
